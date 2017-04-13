1b01612b55561c444ba30518529e0ff97be4c722
Max <durnenko.maxim@yandex.ru>
2017-04-13 20:22:16 +0300

    ff

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(".header .owl-carousel").owlCarousel({
+		items: 1, 
+  })
+})
+
+$(document).ready(function(){
+	$(".footer .owl-carousel").owlCarousel({
+		items: 3,
+		loop: true,
+		dots: true,
+		dotsEach: true, 
+  })
+})
+
